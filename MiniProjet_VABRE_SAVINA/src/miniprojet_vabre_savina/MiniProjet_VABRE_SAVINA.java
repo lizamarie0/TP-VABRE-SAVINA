1a5b1a08c3491106c28c2ffc4c61b46ea5252a6d
Aliénor VABRE <alienor.vabre@epfedu.fr>
2024-11-20 14:38:18 +0100

    Mini Projet

---
+++ MiniProjet_VABRE_SAVINA/src/miniprojet_vabre_savina/MiniProjet_VABRE_SAVINA.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package miniprojet_vabre_savina;
+
+/**
+ *
+ * @author alien
+ */
+public class MiniProjet_VABRE_SAVINA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
