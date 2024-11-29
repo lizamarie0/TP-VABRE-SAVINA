a71aa7343638468139e1a1c1b4bb2879f5fe2478
Aliénor VABRE <alienor.vabre@epfedu.fr>
2024-11-29 20:14:53 +0100

    Mini Projet

--- MiniProjet_VABRE_SAVINA/src/miniprojet_vabre_savina/Partie.java
+++ MiniProjet_VABRE_SAVINA/src/miniprojet_vabre_savina/Partie.java
@@ -5,6 +5,9 @@
  */
 package miniprojet_vabre_savina;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author alien
@@ -12,12 +15,36 @@ package miniprojet_vabre_savina;
 public class Partie {
     
     PlateauDeJeu plateau;
-    List<Character> couleursDisponibles;
+    ArrayList<Character> couleursDisponibles;
     
-    public Partie(int tailleCombinaison, int nbToursMax, List<Character> couleursDisponibles){
+    public Partie(int tailleCombinaison, int nbToursMax, ArrayList<Character> couleursDisponibles){
         this.couleursDisponibles = couleursDisponibles;
-        Combinaison combinaisonSecrete = genereCombinaisonSecrete(tailleCombinaison);
+        Combinaison combinaisonSecrete = Combinaison.genereAleatoire(tailleCombinaison, couleursDisponibles);
         this.plateau = new PlateauDeJeu(combinaisonSecrete, nbToursMax);
     }
     
+     public void afficherRegles() {
+        System.out.println("Règles du jeu :");
+        System.out.println("Vous devez deviner une combinaison secrète composée de couleurs.");
+        System.out.println("Chaque tentative doit être composée de la même taille que la combinaison secrète.");
+        System.out.println("Pour chaque tentative, le système vous donnera des indices :");
+        System.out.println("- Un pion noir signifie que la couleur est bien placée.");
+        System.out.println("- Un pion blanc signifie que la couleur est présente mais mal placée.");
+        System.out.println("Vous avez un nombre limité de tours pour trouver la bonne combinaison.");
+    }
+    
+     public void LancerPartie(){
+         Scanner sc = new Scanner(System.in);
+         int tour = 1;
+         String entree;
+         
+         while (tour <= plateau.nbToursMax){
+             plateau.afficherPlateau();
+            
+             System.out.println("Tour : " + tour + ", proposer une combinaison : ");
+             entree  = sc.nextLine();
+             
+             
+         }
+     }
 }
