89357055b40579cfca52d30d5b430310bb2b4a4a
Aliénor VABRE <alienor.vabre@epfedu.fr>
2024-11-29 15:10:27 +0100

    Mini Projet

--- MiniProjet_VABRE_SAVINA/src/miniprojet_vabre_savina/Combinaison.java
+++ MiniProjet_VABRE_SAVINA/src/miniprojet_vabre_savina/Combinaison.java
@@ -2,6 +2,7 @@ package miniprojet_vabre_savina;
 
 
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.Random;
 import miniprojet_vabre_savina.Pion;
 
@@ -18,27 +19,59 @@ import miniprojet_vabre_savina.Pion;
 public class Combinaison {
     
     Pion [] elements;
-    int taille = elements.length;
+    private int taille;
     
-    public Combinaison ( int taille, Pion [] elements){
-        this.taille=taille;
-        this.elements=elements;
-        
+     public Combinaison(Pion[] elements) {
+        this.elements = elements;
+        this.taille = elements.length;
     }
         
-    public void genereAleatoire(int taille, ArrayList<Character>couleursDisponibles){
+    public static Combinaison genereAleatoire(int taille, ArrayList<Character>couleursDisponibles){
         Random random = new Random();
         Pion [] elements = new Pion[taille];
         
         for(int i = 0; i<taille; i++){
-            
+            char couleur = couleursDisponibles.get(random.nextInt(couleursDisponibles.size()));
+            elements[i] = new Pion(couleur);
         }
+        return new Combinaison(elements);
+    }
+    
+    public int[] comparer(Combinaison autre){
+        int blanc = 0; //Bien placés
+        int noir = 0; //Mal placés
+        
+        boolean [] VisitedAutre = new boolean [autre.taille]; // On marque les pions déjà utilisés dans l'autre combinaison.
+        boolean [] VisitedCurrent = new boolean [this.taille]; //On marque les pions déjà comptés dans la combinaison actuelle.
+        
+        // ces deux tableaux permettent d'éviter des erreurs comme des doublons.
+        
+        for (int i = 0; i < taille; i++) {
+            if (Objects.equals(this.elements[i].getCouleur(), autre.elements[i].getCouleur())) { //Si la couleur du pion est identique dans les deux combinaison pour une même place alors:
+            noir++; //On ajoute un pion noir.
+            VisitedAutre[i] = true; //On note son indice comme visité dans la liste.
+            VisitedCurrent[i] = true; //On note son indice comme visité dans la liste.
+    }
+}
+        for (int i = 0; i < taille; i++) {
+            if (!VisitedCurrent[i]){ //On parcours tous les pions qui n'ont pas été comptés comme noirs avec la précédente boucle.
+                for (int j = 0; j < taille; j++){ 
+                    if(VisitedAutre[i]&&this.elements[i].getCouleur() == autre.elements[j].getCouleur()){ //Pour chaque pions restant, on cherche dans la combinaison "Autre", une couleur qui n'a pas encore été utilisée.
+                        blanc++; //Si c'est vrai, on a un pion blanc en plus.
+                        VisitedAutre[j]=true; // On note son indice comme visité dans la liste.
+                        break;
+                    }
+                }
+            }
+} return new int[] {noir,blanc};
     }
     
      @Override
-    public String toString () {
-    return "la taille est "+ taille + ", et son nombre de element bon est : " + elements;
-    
-    
+    public String toString() {
+    String result = "";
+    for (Pion pion : elements) {
+        result += pion.getCouleur();
+    }
+    return result;
 }
 }
